80086e63cbf7809f5929ad5a3a2dbdfc0028b4cd
Ben Magee <ben@bmagee.com>
2019-05-14 17:32:23 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robovac",
+    version="0.0.1",
+    author="BnMcG",
+    author_email="ben@bmagee.com",
+    description="Python API for communicating with Eufy RoboVac 11c devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bnmcg/pyrobovac",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
