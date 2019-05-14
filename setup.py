019ebdf5b8fbe9a746f01749a0a27699ba7a4e93
Ben Magee <ben@bmagee.com>
2019-05-14 17:41:15 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robovac",
-    version="0.0.2",
+    version="0.0.3",
     author="BnMcG",
     author_email="ben@bmagee.com",
     description="Python API for communicating with Eufy RoboVac 11c devices",
